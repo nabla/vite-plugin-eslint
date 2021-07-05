27667956292ab4ec717017e4becf0b9bcf5d1659
Arnaud Barré <arnaud@nabla.com>
2021-07-05 11:38:43 +0200

    Don't initialize worker for builds
    This was preventing the build from properly exiting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const { resolve } = require("path");
 const { Worker } = require("worker_threads");
 const { normalizePath } = require("vite");
 
@@ -9,15 +9,18 @@ module.exports = function eslintPlugin(options = {}) {
     formatter,
   } = options;
 
-  const worker = new Worker(path.resolve(__dirname, "./worker.js"), {
-    workerData: { options: { cache: true, ...eslintOptions }, formatter },
-  });
+  let worker; // Don't initialize worker for builds
 
   return {
     name: "eslint",
     apply: "serve",
     transform(_code, id) {
       const path = normalizePath(id);
+      if (!worker) {
+        worker = new Worker(resolve(__dirname, "./worker.js"), {
+          workerData: { options: { cache: true, ...eslintOptions }, formatter },
+        });
+      }
       if (shouldLint(path)) worker.postMessage(path);
       return null;
     },
