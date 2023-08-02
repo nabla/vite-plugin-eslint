45618fc634d36ae45d86dfbc76dbada3eb2f631d
Tom Alon <tom@stereokai.com>
2023-08-02 16:49:32 +0200

    Fix bugs + Add quality of life features to worker

--- src/worker.js
+++ src/worker.js
@@ -2,12 +2,30 @@ const { workerData, parentPort } = require("worker_threads");
 const chalk = require("chalk");
 const fs = require("fs").promises;
 const { ESLint } = require("eslint");
-
 const eslint = new ESLint(workerData.options);
-
+const path = require("path");
 const formatterPromise = workerData.formatter
   ? eslint.loadFormatter(workerData.formatter)
   : undefined;
+
+function getPrettyPath(absolutePath) {
+  let relativePath = path.relative(process.cwd(), absolutePath);
+  let containingDir = path.dirname(relativePath);
+
+  if (containingDir && containingDir !== '.') {
+      relativePath = path.join(path.basename(containingDir), path.basename(relativePath));
+  }
+
+  return relativePath;
+}
+
+function logMessage(message) {
+  console.log(
+    chalk.dim(new Date().toLocaleTimeString()),
+    chalk.bold(chalk.cyan("[vite-plugin-eslint]")),
+    message
+  );
+}
 
 parentPort.on("message", (path) => {
   eslint
@@ -15,18 +33,20 @@ parentPort.on("message", (path) => {
     .then(async (ignored) => {
       if (ignored) return;
       const [report] = await eslint.lintFiles(path);
+      if (!report) return;
       if (report.output !== undefined) await fs.writeFile(path, report.output);
       if (report.messages.length === 0) return;
       if (formatterPromise) {
         const formatter = await formatterPromise;
-        console.log(await formatter.format([report]));
+        logMessage(await formatter.format([report]));
       } else {
         report.messages.forEach((m) => {
-          const prettyPath = path.slice(path.indexOf("/src/") + 1);
+          const prettyPath = getPrettyPath(path)
           const location = `${prettyPath}(${m.line},${m.column})`;
           const rule = m.ruleId ? ` ${m.ruleId}` : "";
-          console.log(
-            `${location}: ${chalk[m.severity === 2 ? "red" : "yellow"](
+          const color = chalk[m.severity === 2 ? "red" : "yellow"]
+          logMessage(
+            `${chalk.dim(location + ':')} ${color(
               m.message
             )}${rule}`
           );
@@ -36,8 +56,8 @@ parentPort.on("message", (path) => {
     .catch((e) => {
       if (e.messageTemplate === "file-not-found" && e.messageData?.pattern) {
         // Can happen when the file is deleted or moved
-        console.log(
-          `${chalk.yellow(`[eslint] File not found`)} ${chalk.dim(
+        logMessage(
+          `${chalk.yellow(`File not found`)} ${chalk.dim(
             e.messageData.pattern
           )}`
         );
